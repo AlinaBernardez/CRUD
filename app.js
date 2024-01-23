ac856e96319676944c88643982a493fb670abe20
AlinaB <bernardezalina@gmail.com>
2024-01-23 21:08:33 +0100

    ejercicio

--- app.js
+++ app.js
@@ -5,35 +5,32 @@ const usuarios = require('./users')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send(`
-    <h1>Personajes Street Fighter:</h1>
-    <ul>
-        ${usuarios.map(usuario => `<li>Nombre: ${usuario.nombre} | Edad: ${usuario.edad} | País: ${usuario.lugarProcedencia}</li>`)
-    .join('')}
-    </ul>
-    <h3>Añadir jugador</h3>
-    <form action='/usuarios' method='post'>
-        <label for='nombre' />
-        <input type='text' id='nombre' name='nombre' required placeholder='Nombre' />
-        <label for='edad' />
-        <input type='text' id='edad' name='edad' required placeholder='Edad' />
-        <label for='pais' />
-        <input type='text' id='pais' name='pais' required placeholder='Lugar procedencia' />
-        <button type='submit'>Crear</button>
-    </form>
-    <h3>Buscar jugador</h3>
-    <form action='/usuarios/' method='get'>
-        <label />
-        <input type='text' id='nombre' name='nombre' required />
-        <button type='submit'>Buscar</button>
-    </form>
-    <a href='/usuarios'>Usuarios json<a>
-    `)
-});
-
 app.get('/usuarios', (req, res) => {
-    res.json(usuarios);
+    res.json(usuarios)
+    // res.send(`
+    // <h1>Personajes Street Fighter:</h1>
+    // <ul>
+    //     ${usuarios.map(usuario => `<li>Nombre: ${usuario.nombre} | Edad: ${usuario.edad} | País: ${usuario.lugarProcedencia}</li>`)
+    // .join('')}
+    // </ul>
+    // <h3>Añadir jugador</h3>
+    // <form action='/usuarios' method='post'>
+    //     <label for='nombre' />
+    //     <input type='text' id='nombre' name='nombre' required placeholder='Nombre' />
+    //     <label for='edad' />
+    //     <input type='text' id='edad' name='edad' required placeholder='Edad' />
+    //     <label for='pais' />
+    //     <input type='text' id='pais' name='pais' required placeholder='Lugar procedencia' />
+    //     <button type='submit'>Crear</button>
+    // </form>
+    // <h3>Buscar jugador</h3>
+    // <form action='/usuarios/' method='get'>
+    //     <label />
+    //     <input type='text' id='nombre' name='nombre' required />
+    //     <button type='submit'>Buscar</button>
+    // </form>
+    // <a href='/usuarios'>Usuarios json<a>
+    // `)
 });
 
 app.post('/usuarios', (req, res) => {
@@ -44,43 +41,34 @@ app.post('/usuarios', (req, res) => {
         lugarProcedencia: req.body.pais
     };
     usuarios.push(nuevo);
-    res.redirect('/');
+    res.json(usuarios);
 });
 
 app.get('/usuarios/:nombre', (req, res) => {
     const {nombre} = req.params
     let resultados = usuarios.filter(user => user.nombre == nombre)
-    console.log(resultados)
     if(resultados) {
-        res.send(`
-        <div>
-            ${resultados.map(result => {
-            `<p>${result.nombre}</p>
-            <p>${result.edad}</p>
-            <p>${result.lugarProcedencia}</p>`
-            })}
-        </div>
-        `)
+        res.status(200).json(resultados)
     }
     else {
-        res.send(`
-        <p>Ese jugador no existe</p>
-        `)
+        res.status(500).json({message: 'Error!!'})
     }
 });
 
 app.put('/usuarios/:nombre', (req, res) => {
-    const {nombre} = req.body
+    const {nombre} = req.params
+    const toChange = usuarios.find(usuario => usuario.nombre == nombre)
+    toChange.edad = 35
+    res.json(usuarios)
 });
 
 app.delete('/usuarios/:nombre', (req, res) => {
     const {nombre} = req.params
-
     const nameIndex = usuarios.findIndex(user =>  user.nombre == nombre)
     usuarios.splice(nameIndex, 1)
-    res.send('Juador eliminado')
+    res.json(usuarios)
 });
 
-app.listen(3000, () => {
-    console.log('Express listening http://localhost:3000')
+app.listen(5000, () => {
+    console.log('Express listening http://localhost:5000')
 });
